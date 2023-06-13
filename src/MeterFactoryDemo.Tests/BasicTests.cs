212a7c9191b07836a16970572401dbfd16c54b81
James Newton-King <james@newtonking.com>
2023-06-13 22:40:50 +0800

    Demo

--- src/MeterFactoryDemo.Tests/BasicTests.cs
+++ src/MeterFactoryDemo.Tests/BasicTests.cs
@@ -24,6 +24,9 @@ namespace MeterFactoryDemo.Tests
             // Assert
             Assert.Equal("Hello World!", await response.Content.ReadAsStringAsync());
 
+            // This is currently flaky. Counter value is added once response is complete.
+            // There is a race between the client reading the response + assert and the counter.
+            // Need InstrumentRecorder.WaitForMeasurementsAsync API to make this test work reliably.
             Assert.Collection(instrumentRecorder.GetMeasurements(),
                 measurement =>
                 {
@@ -31,6 +34,44 @@ namespace MeterFactoryDemo.Tests
                     Assert.Equal("GET", measurement.Tags.ToArray().Single(t => t.Key == "method").Value);
                     Assert.Equal("/", measurement.Tags.ToArray().Single(t => t.Key == "route").Value);
                 });
+        }
+
+        [Fact]
+        public async Task Get_MeterListener_RequestCounterIncreased()
+        {
+            // Arrange
+            var requestDurationReceivedTcs = new TaskCompletionSource<Measurement<double>>();
+
+            var meterFactory = _factory.Services.GetRequiredService<IMeterFactory>();
+            var meterListener = new MeterListener();
+            meterListener.InstrumentPublished = (instrument, listener) =>
+            {
+                if (instrument.Meter.Scope == meterFactory &&
+                    instrument.Meter.Name == "Microsoft.AspNetCore.Hosting" &&
+                    instrument.Name == "request-duration")
+                {
+                    listener.EnableMeasurementEvents(instrument);
+                }
+            };
+            meterListener.SetMeasurementEventCallback<double>((instrument, measurement, tags, state) =>
+            {
+                requestDurationReceivedTcs.TrySetResult(new Measurement<double>(measurement, tags));
+            });
+            meterListener.Start();
+
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/");
+
+            // Assert
+            Assert.Equal("Hello World!", await response.Content.ReadAsStringAsync());
+
+            var requestDuration = await requestDurationReceivedTcs.Task;
+
+            Assert.Equal("http", requestDuration.Tags.ToArray().Single(t => t.Key == "scheme").Value);
+            Assert.Equal("GET", requestDuration.Tags.ToArray().Single(t => t.Key == "method").Value);
+            Assert.Equal("/", requestDuration.Tags.ToArray().Single(t => t.Key == "route").Value);
         }
     }
 }
